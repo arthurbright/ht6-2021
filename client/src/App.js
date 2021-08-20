cfc987ceaa4c39e07f4b9fbc5f090651d3d0df6d
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-08-20 16:15:33 -0400

    react stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <p>hi</p>
+      <Header />
+      <p
+        onClick={() => {
+          alert("kachow");
+        }}
+      >
+        Middle
+      </p>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        {count}
+      </button>
+      <Footer companyTitle="egg inc." />
     </div>
   );
 }
