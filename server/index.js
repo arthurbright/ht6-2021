e5be84d926fd5ce2be0a3be7654aba96f6f4bcad
SirTechnical <kevin203@gmail.com>
2021-08-21 17:13:22 -0400

    Sequelize proof-of-concept

--- server/index.js
+++ server/index.js
@@ -1,40 +1,42 @@
-//imports
+// Imports
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const apiRoute = require("./routes/api");
 const geo = require('./util/geo.js');
 
-//instantiate app
+// Instantiate App
 const app = express();
 app.use(express.json());
 app.use("/api", apiRoute);
 
-//for development:
+// For Development:
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-//debug paths
+// Debug Paths
 app.get("/geo", async (req, res)=>{
-  let data = await geo.getDestinations(43.858730, -79.286930, 3000, "park");
+    let data = await geo.getDestinations(43.858730, -79.286930, 3000, "park");
   
-  res.json(data);
+    res.json(data);
 });
 
 app.get("/rev", async (req, res)=>{
-  let data = await geo.getReviews(43.8544773302915,-79.2540960197085, "Boston Pizza");
+    let data = await geo.getReviews(43.8544773302915,-79.2540960197085, "Boston Pizza");
   
-  res.json(data);
+    res.json(data);
 });
 
-//serve html
+// Serve HTML
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
-
-
-//run app
+// Run app
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}`);
 });
+
+
+
+
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,9 @@
-const express = require('express');
+require('dotenv').config();
 
+const express = require('express');
 const router = express.Router();
 
+const fs = require("fs");
 
 // Database Shenanigans
 const parse = require("pg-connection-string").parse;
@@ -10,27 +12,56 @@ const prompt = require("prompt");
 const { execSync } = require("child_process");
 const { v4: uuidv4 } = require("uuid");
 
+// Connecting to CockroachDB through Sequelize
+const Sequelize = require("sequelize-cockroachdb");
+const { prependOnceListener } = require("process");
+var sequelize = new Sequelize({
+    dialect: "postgres",
+    username: "test_app",
+    password: process.env.COCKROACHDBPWD,
+    host: "free-tier.gcp-us-central1.cockroachlabs.cloud",
+    port: 26257,
+    database: "kindling-3054.defaultdb",
+    dialectOptions: {
+        ssl: {
+      
+      //For secure connection:
+        /*ca: fs.readFileSync('YOURPATH/cc-ca.crt')
+            .toString()*/
+        },
+    },
+    logging: false, 
+});
 
 
 
+const Rooms = sequelize.define("rooms", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: Sequelize.TEXT,
+    },
+    phoneNumber: {
+        type: Sequelize.INTEGER,
+    },
+});
 
-// [TESTING] This function is called within the first transaction. It inserts some initial values into the "accounts" table.
-async function initTable(client, callback) {
-    let i = 0;
-    while (i < accountValues.length) {
-        accountValues[i] = await uuidv4();
-        i++;
-    }
-  
-    const insertStatement =
-        "INSERT INTO accounts (id, balance) VALUES ($1, 1000), ($2, 250), ($3, 0);";
-    await client.query(insertStatement, accountValues, callback);
-  
-    const selectBalanceStatement = "SELECT id, balance FROM accounts;";
-    await client.query(selectBalanceStatement, callback);
-}
-
-
+const People = sequelize.define("people", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true, 
+        primaryKey: true,
+    },
+    name: {
+        type: Sequelize.TEXT,
+    },
+    phoneNumber: {
+        type: Sequelize.INTEGER,
+    },
+});
 
 
 // Testing
@@ -46,5 +77,82 @@ router.get('/kevin_test', (req, res)=>{
         pi: 3.14
     })
 });
+
+
+// Add entry to Sequelize DB
+/*
+router.post('/submit', function(req, res) {
+    
+    Rooms.sync({force:false})
+        .then(function () {
+        
+            return Rooms.bulkCreate([
+
+
+            ]);
+        })
+
+        .catch(function (err) {
+            console.error("error: " + err.message);
+        });
+
+        res.send('Submitted Successfully!<br /> [confirmation]');
+
+
+});*/
+//Handle submitted form data
+ 
+router.post('/submit', function (req, res) {
+ 
+    //Get our values submitted from the form
+    var fromName = req.body.name;
+    var fromPhone = req.body.phone;
+ 
+    //Add our POST data to CockroachDB via Sequelize
+    People.sync({
+        force: false,
+    })
+        .then(function () {
+        // Insert new data into People table
+        return People.bulkCreate([
+            {
+            name: fromName,
+            phoneNumber: fromPhone,
+            },
+        ]);
+        })
+ 
+    	  //Error handling for database errors
+        .catch(function (err) {
+        console.error("error: " + err.message);
+        });    
+        
+        //Tell them it was a success
+        res.send('Submitted Successfully!<br /> Name:  ' + fromName + '<br />Phone:  ' + fromPhone);
+});
+
+router.get('/list', (req, res) => {
+ 
+    //Get our data from CockroachDB
+    People.sync({
+         force:false,
+    })
+    .then(function() {
+       return People.findAll();
+    })
+        
+    .then(function (people) {
+        //Render output from CockroachDB using our PUG template
+        //res.render('list', { people : people });
+
+        /*people.forEach(function (person) {
+            res.json(person);
+        });*/
+        res.json(people);
+        
+    })
+ 
+})
+
 
 module.exports = router;
