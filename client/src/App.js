fe22729508681f0fdcbb8400924275ae335f488d
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-08-21 00:46:27 -0400

    made api call on load

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0);
-  // const [data, setData] = useState(null);
 
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     let res = await fetch("/api/hi");
-  //     let resJson = await res.json();
-  //     console.log(resJson);
-  //     setData(resJson);
-  //   }
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    async function fetchData() {
+      let res = await fetch("/api/hi", {
+        method: 'GET'
+      });
+      let resJson = await res.json();
+      console.log(resJson);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
