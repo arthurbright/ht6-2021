a7b7841f4f5df7528c51dc01f0a9a7d519c6c965
SirTechnical <kevin203@gmail.com>
2021-08-21 21:44:11 -0400

    Nodemailer testing

--- server/util/email.js
+++ server/util/email.js
@@ -1,1 +1,33 @@
-//ni hao
+var nodemailer = require('nodemailer');
+
+const email_address = "bonfire.noreply@gmail.com";
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: email_address,
+        pass: process.env.GMAILPWD
+    }
+});
+
+var mailOptions = {
+    from: email_address,
+    to: 'kevin203@gmail.com, artb1234567@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+};
+
+
+
+function sendTestMail() {
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } 
+        else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+module.exports.sendTestMail = sendTestMail;
