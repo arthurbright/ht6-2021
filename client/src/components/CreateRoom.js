582bc8371c0abbd7bb1bbc6177d06b7d976fec81
Lavan Sumanan <lavan.sumanan@gmail.com>
2021-08-21 23:53:11 -0400

    changed things

--- client/src/components/CreateRoom.js
+++ client/src/components/CreateRoom.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { validator } from "sequelize/types/lib/utils/validator-extras";
 import Tags from "./Tags";
 
 const CreateRoom = (props) => {
+  const [value, setValue] = useState(null);
+
+  async function sendOptions() {
+    
+    var userLatitude;
+    var userLongitude;
+    navigator.geolocation.getCurrentPosition((position) => {
+      userLatitude = position.coords.latitude;
+      userLongitude = position.coords.longitude;
+    })
+
+    let activityTypes = value.map((activity) => {
+      return activity.val;
+    });
+
+    let data = {
+      expected_users: 2,
+      email: "lavanpie@gmail.com",
+      location_parameters: {
+        latitude: "",
+        longitude: "",
+        radius: 5000,
+        types: activityTypes,
+        numResults: 10,
+      },
+    };
+
+    console.log(data);
+
+    let url = "/api/create_room";
+    let options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    // let res = await fetch(url, options);
+    // let resJson = await res.json();
+  }
+
   return (
     <div>
       <div className="box"></div>
-      <Tags />
+      <Tags value={value} setValue={setValue} />
       <button
         className="button"
         style={{
           top: "704px",
           background: "rgba(26, 147, 111, 0.6)",
           color: "#ffffff",
+        }}
+        onClick={() => {
+          sendOptions();
         }}
       >
         CREATE ROOM
--- client/src/components/Tags.js
+++ client/src/components/Tags.js
@@ -1,35 +1,27 @@
 /* eslint-disable no-use-before-define */
-import React from "react";
+import React, { useState } from "react";
 import Autocomplete from "@material-ui/lab/Autocomplete";
-import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: 500,
-    "& > * + *": {
-      marginTop: theme.spacing(3),
-    },
-  },
-}));
-
-export default function Tags() {
-  const classes = useStyles();
-
+export default function Tags(props) {
   return (
-    <div className={classes.root}>
+    <div className="selectActivities">
       <Autocomplete
+        onChange={(event, newValue) => {
+          props.setValue(newValue);
+        }}
+        limitTags={1}
         multiple
         id="tags-standard"
         options={activities}
-        getOptionLabel={(option) => option.val}
-        defaultValue={[activities[0]]}
+        getOptionLabel={(option) => option.title}
         renderInput={(params) => (
           <TextField
+            className="selectActivitiesText"
             {...params}
-            variant="standard"
+            variant="outlined"
             label="Select activities"
-            placeholder="val"
+            placeholder="activity"
           />
         )}
       />
@@ -37,48 +29,47 @@ export default function Tags() {
   );
 }
 
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
 const activities = [
-  { val: "amusement_park", title: "Amusement Park"},
-  { val: "aquarium", title: "Aquarium"},
-  { val: "art_gallery", title: "Art Gallery" },
-  { val: "bakery", title: "Bakery" },
-  { val: "bar", title: "Bar" },
-  { val: "beauty_salon", title: "Beauty Salon" },
-  { val: "bicycle_store", title: "Bicycle Store" },
-  { val: "book_store", title: "Book Store" },
-  { val: "bowling_alley", title: "Bowling Alley"},
-  { val: "cafe", title: "Cafe" },
-  { val: "casino", title: "Casino" },
-  { val: "church", title: "Church" },
-  { val: "clothing_store", title: "Clothing Store" },
-  { val: "convenience_store", title: "Convenience Store" },
-  { val: "department_store", title: "Department Store" },
-  { val: "electronics_store", title: "Electronic Store" },
-  { val: "florist", title: "Florist" },
-  { val: "furniture_store", title: "Furniture Store" },
-  { val: "gas_station", title: "Gas Station" },
-  { val: "gym", title: "Gym" },
-  { val: "hair_care", title: "Hair Care" },
-  { val: "hardware_store", title: "Hardware Store"},
-  { val: "home_goods_store", title: "Home Goods Store" },
-  { val: "jewelry_store", title: "Jewelry Store" },
-  { val: "library", title: "Library" },
-  { val: "liquor_store", title: "Liquor Store" },
-  { val: "lodging", title: "Lodging" },
-  { val: "meal_delivery", title: "Meal Delivery" },
-  { val: "meal_takeaway", title: "Meal Takeaway" },
-  { val: "movie_theater", title: "Movie Theater" },
-  { val: "museum", title: "Museum" },
-  { val: "night_club", title: "Night Club" },
-  { val: "park", title: "Park" },
-  { val: "restaurant", title: "Restaurant" },
-  { val: "shoe_store", title: "Shoe Store" },
-  { val: "shopping_mall", title: "Shopping Mall" },
-  { val: "spa", title: "Spa" },
-  { val: "stadium" , title: "Stadium"},
-  { val: "store", title: "Store" },
-  { val: "supermarket", title: "Supermarket" },
-  { val: "tourist_attraction", title: "Tourist Attraction" },
-  { val: "zoo", title: "Zoo" },
+  { title: "Amusement Park", val: "amusement_park" },
+  { title: "Aquarium", val: "aquarium" },
+  { title: "Art Gallery", val: "art_gallery" },
+  { title: "Bakery", val: "bakery" },
+  { title: "Bar", val: "bar" },
+  { title: "Beauty Salon", val: "beauty_salon" },
+  { title: "Bicycle Store", val: "bicycle_store" },
+  { title: "Book Store", val: "book_store" },
+  { title: "Bowling Alley", val: "bowling_alley" },
+  { title: "Cafe", val: "cafe" },
+  { title: "Casino", val: "casino" },
+  { title: "Church", val: "church" },
+  { title: "Clothing Store", val: "clothing_store" },
+  { title: "Convenience Store", val: "convenience_store" },
+  { title: "Department Store", val: "department_store" },
+  { title: "Electronic Store", val: "electronics_store" },
+  { title: "Florist", val: "florist" },
+  { title: "Furniture Store", val: "furniture_store" },
+  { title: "Gas Station", val: "gas_station" },
+  { title: "Gym", val: "gym" },
+  { title: "Hair Care", val: "hair_care" },
+  { title: "Hardware Store", val: "hardware_store" },
+  { title: "Home Goods Store", val: "home_goods_store" },
+  { title: "Jewelry Store", val: "jewelry_store" },
+  { title: "Library", val: "library" },
+  { title: "Liquor Store", val: "liquor_store" },
+  { title: "Lodging", val: "lodging" },
+  { title: "Meal Delivery", val: "meal_delivery" },
+  { title: "Meal Takeaway", val: "meal_takeaway" },
+  { title: "Movie Theater", val: "movie_theater" },
+  { title: "Museum", val: "museum" },
+  { title: "Night Club", val: "night_club" },
+  { title: "Park", val: "park" },
+  { title: "Restaurant", val: "restaurant" },
+  { title: "Shoe Store", val: "shoe_store" },
+  { title: "Shopping Mall", val: "shopping_mall" },
+  { title: "Spa", val: "spa" },
+  { title: "Stadium", val: "stadium" },
+  { title: "Store", val: "store" },
+  { title: "Supermarket", val: "supermarket" },
+  { title: "Tourist Attraction", val: "tourist_attraction" },
+  { title: "Zoo", val: "zoo" },
 ];
